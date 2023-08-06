ad2f8123a718c008c49e6adb651b252cd7c4ff1c
Adil Bazylov <adil.bazylov@gmail.com>
2023-08-07 03:03:56 +0600

    сверстал базовый экран презентации

--- Presentation App/View/PresentationViewController.swift
+++ Presentation App/View/PresentationViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import SnapKit
 
-class PresentationViewController: UIViewController {
+final class PresentationViewController: UIViewController {
+    // MARK: - UI
+    private lazy var slideView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "globe")
+        return imageView
+    }()
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        return button
+    }()
+    
+    private lazy var nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .blue
+        return button
+    }()
 
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
     }
+    
 
-
+    // MARK: - Setup Views
+    private func setupViews() {
+        [slideView,
+         backButton,
+         nextButton].forEach {view.addSubview($0)}
+        view.backgroundColor = .white
+    }
+    
+    // MARK: - Setup Constraints:
+    private func setupConstraints() {
+        
+        slideView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(200)
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(self.view.bounds.width / 6.0)
+            make.bottom.equalToSuperview().offset(-self.view.bounds.height / 6.0)
+            make.size.equalTo(CGSize(width: 100, height: 50))
+        }
+        nextButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-self.view.bounds.width / 6.0)
+            make.bottom.equalToSuperview().offset(-self.view.bounds.height / 6.0)
+            make.size.equalTo(CGSize(width: 100, height: 50))
+        }
+    }
 }
 
