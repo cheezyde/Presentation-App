5dd19ff513374a84ea63f23cde20d99630aea910
Adil Bazylov <adil.bazylov@gmail.com>
2023-08-08 02:44:32 +0600

    cоздал SlidePresenter

--- Presentation App/Presentation/SlidePresenter.swift
+++ Presentation App/Presentation/SlidePresenter.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+class SlidePresenter: SlidePresenterDelegate {
+    private var mediaItems: [Slide] = []
+    private var currentMediaIndex = 0
+    private var view: MediaView?
+
+    init(view: MediaView) {
+        self.view = view
+        // Initialize media items here
+    }
+
+    func loadNextMedia() {
+        currentMediaIndex = (currentMediaIndex + 1) % mediaItems.count
+        view?.showMedia(mediaItems[currentMediaIndex])
+    }
+
+    func loadPreviousMedia() {
+        currentMediaIndex = (currentMediaIndex + mediaItems.count - 1) % mediaItems.count
+        view?.showMedia(mediaItems[currentMediaIndex])
+    }
+}
+
+protocol SlidePresenterDelegate {
+    func loadNextMedia()
+    func loadPreviousMedia()
+}
