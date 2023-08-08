0388e22011275f1c39023de36ca50e67c13df5fc
Adil Bazylov <adil.bazylov@gmail.com>
2023-08-08 22:11:03 +0600

    дальнейшая переработка presenter

--- Presentation App/Presentation/SlidePresenter.swift
+++ Presentation App/Presentation/SlidePresenter.swift
@@ -10,39 +10,53 @@ import Foundation
 class SlidePresenter {
     private let slideService: SlideService
     weak var presentationView: PresentationView?
-    private var currentIndex = 0
-    private var view: MediaView?
-
-    init(view: MediaView, slideService: SlideService) {
-        self.view = view
+    private var slidesTotal = 0
+    private var currentIndex = 0 {
+        didSet {
+            if currentIndex < 0 {
+                currentIndex = 0
+            }
+            if currentIndex > slidesTotal {
+                currentIndex = slidesTotal
+            }
+        }
+    }
+    
+    init(presentationView: PresentationView, slideService: SlideService) {
+        self.presentationView = presentationView
         self.slideService = slideService
     }
-    
-    func setupPresentation() {
-        self.presentationView?.
-    }
-    
+
     func getSlide() {
-        slideService.fetchSlide(index: currentIndex) {
-            self.presentationView?.loadSlide(slide: $0)
-            self.presentationView?.setupButtons()
+        slideService.fetchSlide(index: currentIndex) { slide, total in
+            self.slidesTotal = total
+            self.presentationView?.loadSlide(slide: slide)
+            if self.currentIndex == 0 {
+                self.presentationView?.setupInitialButtons()
+            } else if self.currentIndex == total - 1 {
+                self.presentationView?.setupFinalButtons()
+            } else {
+                self.presentationView?.setupDefaultButtons()
+            }
         }
     }
     
     func loadNextMedia() {
-        currentIndex = (currentIndex + 1) % mediaItems.count
+        currentIndex = (currentIndex + 1) % slidesTotal
         print(currentIndex)
-        view?.showMedia(mediaItems[currentIndex])
+        getSlide()
     }
 
     func loadPreviousMedia() {
-        currentIndex = (currentIndex + mediaItems.count - 1) % mediaItems.count
-        view?.showMedia(mediaItems[currentIndex])
+        currentIndex = (currentIndex + slidesTotal - 1) % slidesTotal
+        getSlide()
     }
 }
 
 
 protocol PresentationView: AnyObject {
-    func setupButtons()
+    func setupInitialButtons()
+    func setupFinalButtons()
+    func setupDefaultButtons()
     func loadSlide(slide: Slide)
 }
